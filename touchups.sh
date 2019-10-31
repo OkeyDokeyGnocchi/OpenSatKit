1037a88ab6567516ed4eaf57f90f88f920124ac7
okeydokeygnocchi <32558326+OkeyDokeyGnocchi@users.noreply.github.com>
2019-10-31 13:07:56 -0700

    Update touchups.sh to include welcome message and custom osk alias via .bashrc

--- touchups.sh
+++ touchups.sh
@@ -3,6 +3,16 @@
 # Runs the commands necessary to finish setting up OpenSatKit
 # Assumes that OSK is in ~/OpenSatKit-master/    (default)
 
+# Create welcome file with OSK reminder.
+touch ~/welcome_file
+echo 'Type osk and hit enter to run OpenSatKit' >> ~/welcome_file
+
+# Create alias in ~/.bashrc and have it cat the welcome file
+echo -e '\n cat ~/welcome_file' >> ~/.bashrc
+echo -e '\n# Aliases created by touchups.sh' >> ~/.bashrc
+echo 'alias osk="ruby ~/OpenSatKit-master/cosmos/Launcher"' >> ~/.bashrc
+source ~/.bashrc
+
 # Make 42 executable
 cd ~/OpenSatKit-master/42
 chmod 557 42
