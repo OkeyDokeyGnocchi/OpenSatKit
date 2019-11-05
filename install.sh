509dca1dfb9dd0ee344598407a9c2fc7d1bd5809
OkeyDokeyGnocchi <32558326+OkeyDokeyGnocchi@users.noreply.github.com>
2019-11-04 19:34:08 -0800

    Update install.sh
    Trying outside of main()

--- install.sh
+++ install.sh
@@ -77,30 +77,6 @@ find ./ -type f -exec dos2unix {} \;
 # Starting with Ubuntu 18.04 this is required to make the ruby environment saved 
 announce "Close this terminal. Open new terminal: cd OpenSatKit-master/cosmos/  and run 'ruby Launcher' "
 
-# Runs the commands necessary to finish setting up OpenSatKit
-# Assumes that OSK is in ~/OpenSatKit-master/    (default)
-
-# Create welcome file with OSK reminder.
-touch ~/welcome_file
-echo -e 'Welcome! Type osk and hit enter to run OpenSatKit.\n' >> ~/welcome_file
-
-# Create alias in ~/.bashrc and have it cat the welcome file
-echo -e '\ncat ~/welcome_file' >> ~/.bashrc
-echo -e '\n# Aliases created by touchups.sh' >> ~/.bashrc
-echo 'alias osk="ruby ~/OpenSatKit-master/cosmos/Launcher"' >> ~/.bashrc
-
-cd ~/
-source .bashrc
-
-# Make 42 executable
-cd ~/OpenSatKit-master/42
-chmod 557 42
-
-# Run bundle install to fix dependencies for "ruby Launcher"
-cd ~/OpenSatKit-master/cosmos
-bundle install
-
-
 return 1
 }
 
@@ -143,5 +119,28 @@ elif [[ -z $(which apt-get) ]]; then # no Ubuntu package manager
    echo "Sorry, OpenSatKit currently only supports Ubuntu. Please use an Ubuntu virtual machine."
    exit 1
 fi
+
+# Runs the commands necessary to finish setting up OpenSatKit
+# Assumes that OSK is in ~/OpenSatKit-master/    (default)
+
+# Create welcome file with OSK reminder.
+touch ~/welcome_file
+echo -e 'Welcome! Type osk and hit enter to run OpenSatKit.\n' >> ~/welcome_file
+
+# Create alias in ~/.bashrc and have it cat the welcome file
+echo -e '\ncat ~/welcome_file' >> ~/.bashrc
+echo -e '\n# Aliases created by touchups.sh' >> ~/.bashrc
+echo 'alias osk="ruby ~/OpenSatKit-master/cosmos/Launcher"' >> ~/.bashrc
+
+cd ~/
+source .bashrc
+
+# Make 42 executable
+cd ~/OpenSatKit-master/42
+chmod 557 42
+
+# Run bundle install to fix dependencies for "ruby Launcher"
+cd ~/OpenSatKit-master/cosmos
+bundle install
 
 main
